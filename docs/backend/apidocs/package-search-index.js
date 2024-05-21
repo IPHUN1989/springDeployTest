632d1bbfc79b4663997f50254c1c6f8f34bdcc9f
IPHUN1989 <105739329+IPHUN1989@users.noreply.github.com>
2024-05-21 11:11:09 +0000

    Deploying to main from  @ 676dd5ada7baeda0a12c6833a5ba1c7370a1f839 🚀

--- docs/backend/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.test.TestDB1"},{"l":"com.test.TestDB1.controller"},{"l":"com.test.TestDB1.db"},{"l":"com.test.TestDB1.service"}];updateSearchResults();
